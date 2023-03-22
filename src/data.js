ab47bdbba51dcd1316d904ec2092705885c1ccee
DrJuanch <juanjo88023@gmail.com>
2023-03-22 03:12:52 -0500

    This is my portfolio

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const projects = [
+  {
+    title: "NFTs MARKETPLACE",
+    subtitle: "Html, bootstrap and css practice",
+    description:
+      "Here I replied a marketplace called Anefty to practice with html and css, z-index, position relative and absolute and much more",
+    image: "./anefty.gif",
+    link: "https://drjuanch.github.io/NFTS_MARKETPLACE/",
+  },
+  {
+    title: "Rick and Morty",
+    subtitle: "React API",
+    description:
+      "I used the rick and morty API to create this beautiful interface were you can see the whole characters of the tv show",
+    image: "",
+    link: "https://drjuanch.github.io/React_Api/",
+  },
+  {
+    title: "To Do app",
+    subtitle: "List of todos",
+    description:
+      "This is an app where you can add, mark as done and delete todos, I did it to improve my skill with javascript and I'll improve it with react",
+    image: "./assets/img/Anefty.gif",
+    link: "https://drjuanch.github.io/ToDo_List/",
+  },
+  {
+    title: "YARD SALE",
+    subtitle: "marketplace with JS",
+    description:
+      "This is a practice with javascript where I improve my skill with DOM manipulation, html and css layout",
+    image: "./assets/img/Anefty.gif",
+    link: "https://drjuanch.github.io/Javascript-Practice/",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "MongoDB",
+  "Node",
+  "Advance MySQL",
+  "Java",
+  "Python",
+  "Tailwind",
+  "Html",
+  "Css"
+];
